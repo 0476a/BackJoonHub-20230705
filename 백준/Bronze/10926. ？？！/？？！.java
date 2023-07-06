7b92b6d5993dc54f291f82c193f6b9b543b87012
ByeongSoo <120405087+0476a@users.noreply.github.com>
2023-07-07 01:52:21 +0900

    [Bronze V] Title: ??!, Time: 200 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+        Scanner scanner = new Scanner(System.in);
+
+        String id = scanner.next();
+
+        String s = "??!";
+
+        System.out.println(id + s);
+
+
+
+    }
+
+
+
+}
