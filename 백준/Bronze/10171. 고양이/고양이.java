d61a59479fb39a52b1b650f7c8d8c80bbffdd53f
ByeongSoo <120405087+0476a@users.noreply.github.com>
2023-07-08 01:54:42 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        
+
+        System.out.print("\\");
+        System.out.print("    ");
+        System.out.print("/");
+        System.out.println("\\");
+        System.out.print(" ");
+        System.out.print(")");
+        System.out.print("  ");
+        System.out.print("(");
+        System.out.print(" ");
+        System.out.print("'");
+        System.out.println(")");
+        System.out.print("(");
+        System.out.print("  ");
+        System.out.print("/");
+        System.out.print("  ");
+        System.out.println(")");
+        System.out.print(" ");
+        System.out.print("\\");
+        System.out.print("(__)");
+        System.out.print("|");
+
+    }
+}
